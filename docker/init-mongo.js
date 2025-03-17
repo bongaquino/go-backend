cb4f25a81b5b8fc672f48db15fbe7bbe0200668c
Alexander Paul Quinit <paulquinit@gmail.com>
2025-03-17 11:33:50 +0800

    Rename 'account_service' database to 'koneksi' and update user credentials; insert sample data into existing databases

--- docker/init-mongo.js
+++ docker/init-mongo.js
@@ -16,29 +16,15 @@ const sampleData = [
   { name: 'Sample Data 3', value: 300 }
 ];
 
-// Create 'account_service' database and user
-db = db.getSiblingDB('account_service');
+// Create 'koneksi' database and user
+db = db.getSiblingDB('koneksi');
 db.createUser({
-  user: 'account_service_user',
-  pwd: 'account_service_password',
-  roles: [{ role: 'readWrite', db: 'account_service' }]
+  user: 'koneksi_user',
+  pwd: 'koneksi_password',
+  roles: [{ role: 'readWrite', db: 'koneksi' }]
 });
-db.data.insertMany(sampleData);
 
-// Create 'dashboard_service' database and user
-db = db.getSiblingDB('dashboard_service');
-db.createUser({
-  user: 'dashboard_service_user',
-  pwd: 'dashboard_service_password',
-  roles: [{ role: 'readWrite', db: 'dashboard_service' }]
-});
-db.data.insertMany(sampleData);
-
-// Create 'backup_service' database and user
-db = db.getSiblingDB('backup_service');
-db.createUser({
-  user: 'backup_service_user',
-  pwd: 'backup_service_password',
-  roles: [{ role: 'readWrite', db: 'backup_service' }]
-});
-db.data.insertMany(sampleData);
+// Insert data into structured collections
+db.account_service.insertMany(sampleData);
+db.dashboard_service.insertMany(sampleData);
+db.backup_service.insertMany(sampleData);
