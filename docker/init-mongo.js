d8fc4c5678b85bd6a565606029b550994d8b9575
Alexander Paul Quinit <paulquinit@gmail.com>
2025-03-17 09:37:40 +0800

    Add RabbitMQ service to docker-compose.yml and initialize multiple databases with users in init-mongo.js

--- docker/init-mongo.js
+++ docker/init-mongo.js
@@ -1,5 +1,6 @@
 db = db.getSiblingDB('admin');
 
+// Ensure root user exists
 if (db.system.users.find({ user: 'root' }).count() === 0) {
   db.createUser({
     user: 'root',
@@ -8,16 +9,36 @@ if (db.system.users.find({ user: 'root' }).count() === 0) {
   });
 }
 
-db = db.getSiblingDB('koneksi');
-
-db.createUser({
-  user: 'koneksi_user',
-  pwd: 'koneksi_password',
-  roles: [{ role: 'readWrite', db: 'koneksi' }]
-});
-
-db.sampleCollection.insertMany([
+// Define sample data
+const sampleData = [
   { name: 'Sample Data 1', value: 100 },
   { name: 'Sample Data 2', value: 200 },
   { name: 'Sample Data 3', value: 300 }
-]);
+];
+
+// Create 'account_service' database and user
+db = db.getSiblingDB('account_service');
+db.createUser({
+  user: 'account_service_user',
+  pwd: 'account_service_password',
+  roles: [{ role: 'readWrite', db: 'account_service' }]
+});
+db.data.insertMany(sampleData);
+
+// Create 'dashboard_service' database and user
+db = db.getSiblingDB('dashboard_service');
+db.createUser({
+  user: 'dashboard_service_user',
+  pwd: 'dashboard_service_password',
+  roles: [{ role: 'readWrite', db: 'dashboard_service' }]
+});
+db.data.insertMany(sampleData);
+
+// Create 'backup_service' database and user
+db = db.getSiblingDB('backup_service');
+db.createUser({
+  user: 'backup_service_user',
+  pwd: 'backup_service_password',
+  roles: [{ role: 'readWrite', db: 'backup_service' }]
+});
+db.data.insertMany(sampleData);
