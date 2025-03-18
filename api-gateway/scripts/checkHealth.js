5f3a74fd97e4c309169e7cdb8f776adccf7ba624
Alexander Paul Quinit <paulquinit@gmail.com>
2025-03-18 11:02:42 +0800

    Update health check configuration and improve service URLs in checkHealth script

--- api-gateway/scripts/checkHealth.js
+++ api-gateway/scripts/checkHealth.js
@@ -1,15 +1,16 @@
 function checkHealth(request, session, config) {
     var services = {
-    "account": "http://account-service:3000",
-    "backup": "http://backup-service:3001",
-    "dashboard": "http://dashboard-service:3002",
-    "notification": "http://notification-service:3003",};
+        "account": "http://account-service:3000",
+        "backup": "http://backup-service:3001",
+        "dashboard": "http://dashboard-service:3002"
+    };
 
     var results = {};
     var allHealthy = true;
 
     for (var service in services) {
         var url = services[service];
+        
         var response = TykMakeHttpRequest({
             Method: "GET",
             Headers: {
