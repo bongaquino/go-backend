3969ada54facf2e43a7966a95e5f8699619b00cb
Alexander Paul Quinit <paulquinit@gmail.com>
2025-03-19 17:04:06 +0800

    Add Redis configuration and service implementation

---
+++ services/iam/config/redis.go
@@ -0,0 +1,22 @@
+package config
+
+import "koneksi/services/iam/core/env"
+
+// RedisConfig holds the Redis configuration
+type RedisConfig struct {
+	RedisHost     string
+	RedisPort     int
+	RedisPassword string
+}
+
+func LoadRedisConfig() *RedisConfig {
+	// Load environment variables
+	envVars := env.LoadEnv()
+
+	// Create the configuration from environment variables
+	return &RedisConfig{
+		RedisHost:     envVars.RedisHost,
+		RedisPort:     envVars.RedisPort,
+		RedisPassword: envVars.RedisPassword,
+	}
+}
