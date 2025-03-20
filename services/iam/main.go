bcf7181d7224eac1609704ae03702abfb26c42f8
Alexander Paul Quinit <paulquinit@gmail.com>
2025-03-20 18:31:29 +0800

    Remove Swagger documentation and related routes from IAM service

--- services/iam/main.go
+++ services/iam/main.go
@@ -1,16 +1,8 @@
 package main
 
 import (
-	_ "koneksi/services/iam/docs"
 	"koneksi/services/iam/start"
 )
-
-// @title Koneksi IAM Service
-// @version 1.0
-// @description This is the API documentation for Koneksi IAM Service.
-
-// @host localhost:8080
-// @BasePath /
 
 func main() {
 	// Initialize the server
--- services/iam/start/routes.go
+++ services/iam/start/routes.go
@@ -4,18 +4,10 @@ import (
 	ioc "koneksi/services/iam/core/container"
 
 	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // RegisterRoutes sets up the application's routes
 func RegisterRoutes(engine *gin.Engine, container *ioc.Container) {
-	// Swagger Route
-	engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-
-	// Health Check Route
-	engine.GET("/check-health", container.CheckHealthController.Handle)
-
 	// User Routes
 	userGroup := engine.Group("/users")
 	{
