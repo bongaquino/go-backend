e08dbf028b79ee64d60704fac29630297bdbbf74
Alexander Paul Quinit <paulquinit@gmail.com>
2025-06-04 11:11:01 +0800

    Add user configuration settings to health check response

--- server/app/controller/constants/fetch_controller.go
+++ server/app/controller/constants/fetch_controller.go
@@ -5,6 +5,7 @@ import (
 
 	"koneksi/server/app/helper"
 	"koneksi/server/app/service"
+	"koneksi/server/config"
 
 	"github.com/gin-gonic/gin"
 )
@@ -44,10 +45,30 @@ func (fc *FetchController) Handle(ctx *gin.Context) {
 		return
 	}
 
+	userConfig := config.LoadUserConfig()
+	settings := gin.H{
+		"backup_cycle_options":                  userConfig.BackupCycleOptions,
+		"notifications_frequency_options":       userConfig.NotificationsFrequencyOptions,
+		"recovery_priority_order_options":       userConfig.RecoveryPriorityOrderOptions,
+		"recovery_custom_order_options":         userConfig.RecoveryCustomOrderOptions,
+		"default_bytes_limit":                   userConfig.DefaultBytesLimit,
+		"default_backup_cycle":                  userConfig.DefaultBackupCycle,
+		"default_backup_custom_day":             userConfig.DefaultBackupCustomDay,
+		"default_notifications_frequency":       userConfig.DefaultNotificationsFrequency,
+		"default_recovery_priority_order":       userConfig.DefaultRecoveryPriorityOrder,
+		"default_recovery_custom_order":         userConfig.DefaultRecoveryCustomOrder,
+		"default_is_mfa_enabled":                userConfig.DefaultIsMFAEnabled,
+		"default_is_realtime_backup_enabled":    userConfig.DefaultIsRealtimeBackupEnabled,
+		"default_is_email_notification_enabled": userConfig.DefaultIsEmailNotificationEnabled,
+		"default_is_sms_notification_enabled":   userConfig.DefaultIsSMSNotificationEnabled,
+		"default_is_version_history_enabled":    userConfig.DefaultIsVersionHistoryEnabled,
+	}
+
 	// Respond with success
 	helper.FormatResponse(ctx, "success", http.StatusOK, nil, gin.H{
 		"roles":       roles,
 		"policies":    policies,
 		"permissions": permissions,
+		"settings":    settings,
 	}, nil)
 }
